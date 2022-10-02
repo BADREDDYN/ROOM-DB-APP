f8581d41f867f349e104ce3cecd2162513b5f930
BADREDDYN <badreddynelhirch@gmail.com>
2022-10-02 17:15:03 +0100

    Entity[convert data class to sql table].

--- app/src/main/java/com/roomdb/app/model/Post.kt
+++ app/src/main/java/com/roomdb/app/model/Post.kt
@@ -1,6 +1,11 @@
 package com.roomdb.app.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "posts_table")
 data class Post(
+    @PrimaryKey(autoGenerate = true)
     val id: Int,
     val userId: Int,
     var title: String,
