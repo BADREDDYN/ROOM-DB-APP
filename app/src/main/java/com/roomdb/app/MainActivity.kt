2382b6aca9e9c79acc8fa0a848833dfcb606736f
BADREDDYN <badreddynelhirch@gmail.com>
2022-10-02 18:49:16 +0100

    Database

--- app/src/main/java/com/roomdb/app/MainActivity.kt
+++ app/src/main/java/com/roomdb/app/MainActivity.kt
@@ -2,10 +2,15 @@ package com.roomdb.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.roomdb.app.model.local.PostEntity
+import com.roomdb.app.model.local.PostsDatabase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
 }
--- app/src/main/java/com/roomdb/app/model/local/PostDao.kt
+++ app/src/main/java/com/roomdb/app/model/local/PostDao.kt
@@ -1,13 +1,16 @@
 package com.roomdb.app.model.local
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 
 @Dao
-interface PostsDao {
+interface PostDao {
     @Insert
     fun insertPost(post: PostEntity)
     @Query("select * from posts_table;")
     fun getPosts() :List<PostEntity>
+    @Delete
+    fun deletePost(post: PostEntity)
 }
